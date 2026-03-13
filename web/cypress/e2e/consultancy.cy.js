848f2dc28cc9a03e5daa8b24d7cb00deeb7c7fee
Rodrigo Grillo <rodrigo.grillo@gmail.com>
2026-03-13 11:12:32 -0400

    conteudo até o módulo advanced

---
+++ web/cypress/e2e/consultancy.cy.js
@@ -0,0 +1,45 @@
+import { personal, company } from '../fixtures/consultancy.json'
+
+describe('Formulário de Consultoria', () => {
+
+    beforeEach(() => {
+        cy.login()
+        cy.goTo('Formulários', 'Consultoria')
+    })
+
+    it('Deve solicitar consultoria Individual', () => {
+        cy.fillConsultancyForm(personal)
+        cy.submitConsultancyForm()
+        cy.validateConsultancyModal()
+
+
+    })
+
+    it('Deve solicitar consultoria In Company', () => {
+        cy.fillConsultancyForm(company)
+        cy.submitConsultancyForm()
+        cy.validateConsultancyModal()
+
+    })
+
+    it('Deve verificar os campos obrigatórios', () => {
+        cy.submitConsultancyForm()
+
+        const requiredFields = [
+            { label: 'Nome Completo', message: 'Campo obrigatório' },
+            { label: 'Email', message: 'Campo obrigatório' },
+            { label: 'termos de uso', message: 'Você precisa aceitar os termos de uso' }
+        ]
+
+        requiredFields.forEach(({ label, message }) => {
+            cy.contains('label', label)
+                .parent()
+                .find('p')
+                .should('be.visible')
+                .should('have.text', message)
+                .and('have.class', 'text-red-400')
+                .and('have.css', 'color', 'rgb(248, 113, 113)')
+        })
+    })
+})
+
---
+++ web/cypress/e2e/links.cy.js
@@ -0,0 +1,22 @@
+describe('Links abrindo nova guia/janela', ()=> {
+   
+    beforeEach(()=> {
+        cy.login()
+
+    })
+    it('Validando o atributo do link no Instagram', ()=> {
+        cy.get('[data-cy="instagram-link"]')
+        .should('have.attr', 'href', 'https://www.instagram.com/qapapito')
+        .and('have.attr', 'target', '_blank')
+    })
+
+    it('Acessa link de termos de uso removendo o target blank', ()=> {
+        cy.contains('Formulários').click()
+        cy.contains('a', 'termos de uso')
+          .invoke('removeAttr', 'target')
+          .click()
+
+        cy.contains('Ao acessar e usar nossos serviços, você concorda em cumprir estes termos de uso. Se você não concordar com algum aspecto destes termos, não utilize nossos serviços.')
+          .should('be.visible')
+    })
+})
